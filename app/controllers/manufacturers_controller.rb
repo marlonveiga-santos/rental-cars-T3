5ad55b08e5837976718fcdbe70a8c93067c4ed58
marlon <marlon_veiga@hotmail.com>
2020-04-25 16:31:02 -0300

    Tarefa: Editar um fabricante concluída.

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -14,12 +14,34 @@ class ManufacturersController < ApplicationController
     end
 
     def create
-        @manufacturer = Manufacturer.new(params.require(:manufacturer).permit(:name)) 
+        @manufacturer = Manufacturer.new(manufacturer_params) 
         if @manufacturer.save
         redirect_to manufacturer_path(id: @manufacturer.id)
         else
             render :new
         end
+    end
+
+    def edit
+        id = params[:id]
+        @manufacturer = Manufacturer.find(id)
+    end
+
+    def update
+        id = params[:id]
+        @manufacturer = Manufacturer.find(id)
+        @manufacturer.update(manufacturer_params)
+        if @manufacturer.save
+        redirect_to @manufacturer
+    else
+        render :edit
+    end
+    end
+
+    private
+
+    def manufacturer_params
+        params.require(:manufacturer).permit(:name)
     end
 
 end
